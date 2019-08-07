5773237d12a50c0a2805ab10b249c77e686368b8
GGraziadei <48211703+GGraziadei@users.noreply.github.com>
2019-08-07 21:38:55 +0200

    Create coockie-block.php

---
+++ coockie-block.php
@@ -0,0 +1,13 @@
+$dirty = false;
+foreach(headers_list() as $header) {
+    if($dirty) continue; // I already know it needs to be cleaned
+    if(preg_match('/Set-Cookie/',$header)) $dirty = true;
+}
+if($dirty) {
+    $phpversion = explode('.',phpversion());
+    if($phpversion[1] >= 3) {
+        header_remove('Set-Cookie'); // php 5.3
+    } else {
+        header('Set-Cookie:'); // php 5.2
+    }        
+}
